1d50a14febfda41de85a7bedfac48a51e78d534b
sandhya-rathlavath <sandhya2329223@gmail.com>
2021-02-14 23:25:54 -0500

    added edittraining activity

---
+++ app/src/main/java/com/fitnessapp/activities/EditTrainingActivity.java
@@ -0,0 +1,33 @@
+package com.fitnessapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.fitnessapp.R;
+
+public class EditTrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_training);
+
+
+        getSupportActionBar().setTitle("Edit Training");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
