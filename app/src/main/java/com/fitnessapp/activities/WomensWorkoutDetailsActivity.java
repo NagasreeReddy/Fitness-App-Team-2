c267a883594dcb00c2e81ab8123bd166b63308b5
NagasreeReddy <nagasreecharabudla@gmail.com>
2021-02-17 21:09:00 -0500

    WomensWorkoutDetailsActivity and layout updated

---
+++ app/src/main/java/com/fitnessapp/activities/WomensWorkoutDetailsActivity.java
@@ -0,0 +1,45 @@
+package com.fitnessapp.activities;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.fitnessapp.R;
+
+public class WomensWorkoutDetailsActivity extends AppCompatActivity {
+    String URL="http://getfitt.club/getfit/";
+    TextView tv_Workoutname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_womens_workout_details);
+
+        getSupportActionBar().setTitle("Workouts");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageView imageview=(ImageView)findViewById(R.id.imageview);
+        Glide.with(this).load(URL+getIntent().getStringExtra("image")).into(imageview);
+
+        TextView tv_Workoutname=(TextView)findViewById(R.id.tv_Workoutname);
+        TextView tvWorkoutDesc=(TextView)findViewById(R.id.tvWorkoutDesc);
+        tv_Workoutname.setText(getIntent().getStringExtra("wname"));
+        tvWorkoutDesc.setText(getIntent().getStringExtra("desc"));
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
