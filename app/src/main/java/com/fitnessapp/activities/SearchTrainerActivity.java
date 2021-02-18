c166922705aecb7d8fe59d80878cb32645ce4ad3
manikiran1 <b.manikiran@gmail.com>
2021-02-18 12:28:09 -0500

    addedsearchtrainer.java

---
+++ app/src/main/java/com/fitnessapp/activities/SearchTrainerActivity.java
@@ -0,0 +1,32 @@
+package com.fitnessapp.activities;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.fitnessapp.R;
+
+public class SearchTrainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_trainer);
+
+
+        getSupportActionBar().setTitle("Search Trainer");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
