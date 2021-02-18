863de035e9eafb91a104e84909e6d2ce2256dad2
sukhdeepkaur1234 <kaursukhdeep3011@gmail.com>
2021-02-17 21:26:59 -0500

    TrainersInfoActivity and layout updated

---
+++ app/src/main/java/com/fitnessapp/activities/TrainersInfoActivity.java
@@ -0,0 +1,113 @@
+package com.fitnessapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.fitnessapp.R;
+import com.fitnessapp.Utils;
+import com.fitnessapp.api.ApiService;
+import com.fitnessapp.api.RetroClient;
+import com.fitnessapp.models.ResponseData;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class TrainersInfoActivity extends AppCompatActivity {
+    TextView tvName,tvGender,tvEmail,tvExperience,tvDob;
+    RatingBar ratingBar;
+    TextInputEditText etMessage;
+    ProgressDialog pd;
+    String session,Date;
+    SharedPreferences sharedPreferences;
+    Button btnBook;
+    Spinner spinTimings;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainers_info);
+
+
+        sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+         session = sharedPreferences.getString("user_name", "def-val");
+        Toast.makeText(this, ""+session+Date, Toast.LENGTH_SHORT).show();
+
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+         Date = df.format(c);
+
+        tvName=(TextView)findViewById(R.id.tvName);
+        tvGender=(TextView)findViewById(R.id.tvGender);
+        tvEmail=(TextView)findViewById(R.id.tvEmail);
+        tvExperience=(TextView)findViewById(R.id.tvExperience);
+        tvDob=(TextView)findViewById(R.id.tvDob);
+        etMessage=(TextInputEditText)findViewById(R.id.etMessage);
+        spinTimings=(Spinner)findViewById(R.id.spinTimings);
+
+        tvName.setText("Name  :"+getIntent().getStringExtra("name"));
+        tvGender.setText("Gender  :"+getIntent().getStringExtra("gender"));
+        tvEmail.setText("Email  :"+getIntent().getStringExtra("email"));
+        tvExperience.setText("Experience  :"+getIntent().getStringExtra("experience"));
+        tvDob.setText("Date of Birth  :"+getIntent().getStringExtra("dob"));
+
+        ratingBar=(RatingBar)findViewById(R.id.ratingBar);
+        ratingBar.setRating(Float.parseFloat(getIntent().getStringExtra("rating")));
+
+        btnBook=(Button)findViewById(R.id.btnBook);
+        btnBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bookATrainer();
+            }
+        });
+
+    }
+
+    public  void bookATrainer() {
+        pd= new ProgressDialog(TrainersInfoActivity.this);
+        pd.setTitle("Please wait,Data is being submit...");
+        pd.show();
+        ApiService apiService = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = apiService.booktrainer(session,getIntent().getStringExtra("email"),etMessage.getText().toString(),Date,spinTimings.getSelectedItem().toString(),"Booked");
+
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                pd.dismiss();
+                if (response.body().status.equals("true")) {
+                    Toast.makeText(TrainersInfoActivity.this, "Trainer Booked Succussfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(TrainersInfoActivity.this, GetVerifiedTrainersActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(TrainersInfoActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                pd.dismiss();
+                Toast.makeText(TrainersInfoActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
