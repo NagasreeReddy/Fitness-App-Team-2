71be3206629c58d46286f8e893915938850f83cd
sukhdeepkaur1234 <kaursukhdeep3011@gmail.com>
2021-03-04 18:22:56 -0500

    MyFeedBackActivity.java with api connection linked

--- app/src/main/java/com/fitnessapp/activities/MyFeedBackActivity.java
+++ app/src/main/java/com/fitnessapp/activities/MyFeedBackActivity.java
@@ -2,6 +2,7 @@ package com.fitnessapp.activities;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -18,6 +19,7 @@ import com.fitnessapp.api.ApiService;
 import com.fitnessapp.api.RetroClient;
 import com.fitnessapp.models.MyBookingsPojo;
 import com.fitnessapp.models.MyFeedbackPojo;
+import com.fitnessapp.models.ResponseData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -55,4 +57,38 @@ public class MyFeedBackActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(MyFeedBackActivity.this);
         progressDialog.setMessage("Loading....");
         progressDialog.show();
+        ApiService apiService = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = apiService.addfeedback(session,getIntent().getStringExtra("name"),etMessage.getText().toString(),etName.getText().toString(), String.valueOf(ratingBar.getRating()));
 
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                pd.dismiss();
+                if (response.body().status.equals("true")) {
+                    Toast.makeText(GiveFeedbackActivity.this, "Feedback Given Succussfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(GiveFeedbackActivity.this, UserBookingsActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(GiveFeedbackActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                pd.dismiss();
+                Toast.makeText(GiveFeedbackActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
