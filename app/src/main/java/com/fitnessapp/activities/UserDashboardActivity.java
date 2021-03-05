82282b52d8549dc3c80b29fcbb1fa5b97f75da4b
Mani Kiran <b.manikiran@gmail.com>
2021-03-05 00:48:05 -0500

    changes in UserDashboardActivity.java

--- app/src/main/java/com/fitnessapp/activities/UserDashboardActivity.java
+++ app/src/main/java/com/fitnessapp/activities/UserDashboardActivity.java
@@ -1,16 +1,16 @@
 package com.fitnessapp.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 
 import com.fitnessapp.R;
 import com.google.android.material.navigation.NavigationView;
@@ -19,7 +19,7 @@ public class UserDashboardActivity extends AppCompatActivity {
     private ActionBarDrawerToggle t;
     private NavigationView navigation_view;
     private DrawerLayout layout_drawer;
-    CardView cdMyProfile,cdworkoutplans,cdTrainers;
+    CardView cdMyProfile,cdworkoutplans,cdTrainers,cdDitePlan,cdMyGoal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +31,7 @@ public class UserDashboardActivity extends AppCompatActivity {
         cdMyProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(UserDashboardActivity.this,EditProfileActivity.class);
+                Intent intent=new Intent(UserDashboardActivity.this, EditProfileActivity.class);
                 startActivity(intent);
 
             }
@@ -41,7 +41,7 @@ public class UserDashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intent=new Intent(UserDashboardActivity.this,WorkoutPlanActivity.class);
+                Intent intent=new Intent(UserDashboardActivity.this, WorkoutPlanActivity.class);
                 startActivity(intent);
 
             }
@@ -50,7 +50,27 @@ public class UserDashboardActivity extends AppCompatActivity {
         cdTrainers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(UserDashboardActivity.this,GetVerifiedTrainersActivity.class);
+                Intent intent=new Intent(UserDashboardActivity.this, GetVerifiedTrainersActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        cdDitePlan=(CardView)findViewById(R.id.cdDitePlan);
+        cdDitePlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(UserDashboardActivity.this,GetDitePlanActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        cdMyGoal=(CardView)findViewById(R.id.cdMyGoal);
+        cdMyGoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(UserDashboardActivity.this, MyGoalActyvity.class);
                 startActivity(intent);
 
             }
@@ -59,7 +79,7 @@ public class UserDashboardActivity extends AppCompatActivity {
 
     private void navigationView(){
         layout_drawer = (DrawerLayout)findViewById(R.id.layout_drawer);
-        t = new ActionBarDrawerToggle(this, layout_drawer,R.string.Open, R.string.Close);
+        t = new ActionBarDrawerToggle(this, layout_drawer, R.string.Open, R.string.Close);
         layout_drawer.addDrawerListener(t);
         t.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
